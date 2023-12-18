35ff8964ce9baac3d31ae33f55ebff0fae5bc76c
mmehtapavci <mmmehtapavci@gmail.com>
2023-12-18 22:08:52 +0300

    companenet was created

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import User from "./companent/User";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Merhaba React</h1>
-      </div>
+        <h4>App Component</h4>
+        <User />
+      </div >
     );
   }
 }

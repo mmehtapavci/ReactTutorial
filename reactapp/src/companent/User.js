8932eba981d71d68ea80fa0e3d5d4b6b1a20bd27
mmehtapavci <mmmehtapavci@gmail.com>
2023-12-23 14:00:06 +0300

    props-3

--- reactapp/src/companent/User.js
+++ reactapp/src/companent/User.js
@@ -1,20 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class User extends Component {
     render() {
-
-        //Destructing
+        // Destructuring
         const { name, department, salary } = this.props;
-
         return (
-            <div>
-                <ul>
-                    <li>İsim : {name}</li>
-                    <li>Departman : {department} </li>
-                    <li>Maaş :{salary} </li>
-                </ul>
+            <div className="col-md-8 mb-4">
+                <div className="card">
+                    <div className="card-header d-flex justify-content-between">
+                        <h4 className="d-inline">{name}</h4>
+                        <i className="far fa-trash-alt" style={{ cursor: "pointer" }}></i>
+                    </div>
+                    <div className="card-body">
+                        <p className="card-text">Maaş: {salary}</p>
+                        <p className="card-text">Department: {department}</p>
+                    </div>
+                </div>
             </div>
-        )
+        );
     }
 }
+
+// defaultProps ve propTypes tanımlamalarını buraya alın
+User.defaultProps = {
+    name: "Bilgi Yok",
+    salary: "Bilgi Yok",
+    department: "Bilgi Yok",
+};
+
+User.propTypes = {
+    name: PropTypes.string.isRequired,
+    salary: PropTypes.number.isRequired,
+    department: PropTypes.string.isRequired,
+};
+
 export default User;
